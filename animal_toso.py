4611468f5f9e1276b61a0646be5762a24ac70a59
c0b22019 <c0b220193a@edu.teu.ac.jp>
2023-12-20 14:15:42 +0900

    友達の追加：完成

--- animal_toso.py
+++ animal_toso.py
@@ -10,6 +10,19 @@ WIDTH = 1600  # ゲームウィンドウの幅
 HEIGHT = 900  # ゲームウィンドウの高さ
 MAIN_DIR = os.path.split(os.path.abspath(__file__))[0]
 
+def check_bound(obj: pg.Rect) -> tuple[bool, bool]:
+    """
+    オブジェクトが画面内か画面外かを判定し，真理値タプルを返す
+    引数 obj：オブジェクト（爆弾，こうかとん，ビーム）SurfaceのRect
+    戻り値：横方向，縦方向のはみ出し判定結果（画面内：True／画面外：False）
+    """
+    yoko, tate = True, True
+    if obj.left < 0 or WIDTH < obj.right:  # 横方向のはみ出し判定
+        yoko = False
+    if obj.top < 0 or HEIGHT < obj.bottom:  # 縦方向のはみ出し判定
+        tate = False
+    return yoko, tate
+
 class Siro(pg.sprite.Sprite):
     def __init__(self, num: int, zahyo: int, size: float):
         """
@@ -22,7 +35,7 @@ class Siro(pg.sprite.Sprite):
         self.image = self.img
         self.rect = self.image.get_rect()
         self.rect.center = zahyo, 550
-        self.hp = 5000
+        self.hp = 1000
 
 
     def update(self, screen: pg.Surface):
@@ -31,21 +44,81 @@ class Siro(pg.sprite.Sprite):
         """
         screen.blit(self.image, self.rect)
 
+class Tomo(pg.sprite.Sprite):
+    """
+    味方に関するクラス
+    """
+    def __init__(self, name):
+        super().__init__()
+        self.tmr = random.randint(1, 100)
+        self.zahyo = random.randint(1, 15)
+
+        img = pg.transform.rotozoom(pg.image.load(f"{MAIN_DIR}/fig/{name}.png"), 0, 0.5)
+        self.image = img  #猫の画像を読み込む
+        self.rect = self.image.get_rect()  #れくとを生成
+        self.rect.center = 1400, 627-self.zahyo
+        self.speed = 2
+        self.state = "normal"  #猫の状態を定義
+        self.hp = 500
+        self.attack = 20
+    
+    def update(self, screen:pg.Surface):
+        """
+        猫を自陣から左に移動させる。
+        """
+        if self.state == "normal":
+            self.rect.move_ip(-self.speed, 0)
+            screen.blit(self.image, self.rect)
+            if check_bound(self.rect) != (True, True):
+                self.kill()
+        elif self.state == "atk":
+            screen.blit(self.image, self.rect)
+        elif self.state == "death":
+            screen.blit(self.image, self.rect)
+            self.kill()
+
+        
+
 def main():
     pg.display.set_caption("アニマル闘争")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load(f"{MAIN_DIR}/fig/haikei.jpg")
+    cats = pg.sprite.Group()
 
     tmr = 0
     clock = pg.time.Clock()
 
     enemy_siro = Siro(0, 200, 0.4)
     siro = Siro(1, 1400, 0.3)
-    while True:                 
+    while True:      
+        key_lst = pg.key.get_pressed()
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return 0
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_1:
+                    cats.add(Tomo("cat"))  #ねこを追加
+        
+        for cat in cats:
+            if len(pg.sprite.spritecollide(enemy_siro, [cat], False)) != 0:  # ねこと敵城がぶつかったときに止まり攻撃
+                cat.state = "atk"
+                if tmr%100 == cat.tmr:
+                    enemy_siro.hp -= cat.attack
+                    cat.rect.move_ip(8, 0)
+                pg.display.update()
+            else:
+                cat.state = "normal"
+
+        if enemy_siro.hp <= 0:
+            return
+            
+
+
         screen.blit(bg_img, [0, 0])
 
         enemy_siro.update(screen)
         siro.update(screen)
+        cats.update(screen)
         pg.display.update()
 
         tmr += 1
