36b9c94450164f6944241c27c80b801d4384e8c5
c0b22031 <c0b22031a6@edu.teu.ac.jp>
2023-12-20 14:04:09 +0900

    追加機能　実装途中

--- animal_toso.py
+++ animal_toso.py
@@ -11,6 +11,8 @@ HEIGHT = 900  # ゲームウィンドウの高さ
 MAIN_DIR = os.path.split(os.path.abspath(__file__))[0]
 
 class Siro(pg.sprite.Sprite):
+   
+
     def __init__(self, num: int, zahyo: int, size: float):
         """
         城を置く
@@ -31,6 +33,27 @@ class Siro(pg.sprite.Sprite):
         """
         screen.blit(self.image, self.rect)
 
+
+class Cannon(pg.sprite.Sprite): #大砲について
+    def __init__(self):
+        super().__init__()
+        self.image = pg.transform.rotozoom(pg.image.load(f"{MAIN_DIR}/fig/beam.png"), 0, 1.0) #ビームの画像を倍率1倍で挿入している
+        self.image = pg.transform.flip(self.image, True, False) #ビームを左右反転させている
+        self.rect = self.image.get_rect()
+        self.rect.center = (1400, 550) #ビームの座標
+        self.speed = 10 #ビームのスピード
+        self.fired = False
+        
+        
+
+
+    def update(self, screen):
+        if self.fired:
+            screen.blit(self.image, self.rect) #ビームの描画
+            self.rect.move_ip(-self.speed, 0) #ビームの動き
+
+
+
 def main():
     pg.display.set_caption("アニマル闘争")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -39,10 +62,28 @@ def main():
     tmr = 0
     clock = pg.time.Clock()
 
+    cannon = Cannon()
+    font = pg.font.SysFont("hgp創英角ﾎﾟｯﾌﾟ体", 32) #フォントのサイズを32にする
+    text = font.render('大砲完了', True, (0, 0, 0)) #大砲完了と表示させる
+
+
     enemy_siro = Siro(0, 200, 0.4)
     siro = Siro(1, 1400, 0.3)
-    while True:                 
+    while True:  
+        key_lst = pg.key.get_pressed()
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return 0         
+            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+                cannon.fired = True
+                
+        
         screen.blit(bg_img, [0, 0])
+
+        if tmr >=5:
+            screen.blit(text, (800, 500))
+            
+        cannon.update(screen)
 
         enemy_siro.update(screen)
         siro.update(screen)
@@ -50,6 +91,9 @@ def main():
 
         tmr += 1
         clock.tick(50)
+
+            
+                
 
 
 
