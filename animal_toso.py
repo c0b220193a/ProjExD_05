01bfdc6ab9e78f7c33276b6e34dd30926827bc62
C0B22007 <c0b2200787@edu.teu.ac.jp>
2023-12-21 10:42:37 +0900

    追加機能城のHPの追加

--- animal_toso.py
+++ animal_toso.py
@@ -11,20 +11,23 @@ HEIGHT = 900  # ゲームウィンドウの高さ
 MAIN_DIR = os.path.split(os.path.abspath(__file__))[0]
 
 
-
 class Siro(pg.sprite.Sprite):
-    def __init__(self, num: int, zahyo: int, size: float):
+    def __init__(self, num: int, zahyo: int, size: float, iro):
         """
         城を置く
         """
         super().__init__()
         img0 = pg.transform.rotozoom(pg.image.load(f"{MAIN_DIR}/fig/siro{num}.png"), 0, size)
         self.img = pg.transform.flip(img0, True, False)  # デフォルトの城
-
+        self.font = pg.font.SysFont("hgp創英角ﾎﾟｯﾌﾟ体", 20)
         self.image = self.img
         self.rect = self.image.get_rect()
         self.rect.center = zahyo, 550
         self.hp = 5000
+
+        self.change_HP = self.font.render(f"{self.hp}/5000", 0, iro)
+        self.rect_HP = self.change_HP.get_rect()
+        self.rect_HP.center = zahyo, 420
 
 
     def update(self, screen: pg.Surface):
@@ -32,6 +35,8 @@ class Siro(pg.sprite.Sprite):
         城の描画判定
         """
         screen.blit(self.image, self.rect)
+        screen.blit(self.change_HP,self.rect_HP)
+
 class Money:
     def __init__(self,amount):
         self.amount=amount
@@ -59,8 +64,8 @@ def main():
     money = Money(0)
 
 
-    enemy_siro = Siro(0, 200, 0.4)
-    siro = Siro(1, 1400, 0.3)
+    enemy_siro = Siro(0, 200, 0.4,(0,0,0))
+    siro = Siro(1, 1400, 0.3,(255,255,255))
     while True:
         key_lst = pg.key.get_pressed()
         for event in pg.event.get():
