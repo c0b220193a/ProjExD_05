0742be68e64b58c6f8cbe504cf66d882b0aec4de
C0B22123 <C0B22123d2@edu.teu.ac.jp>
2023-12-21 10:30:07 +0900

    攻撃エフェクト追加

--- animal_toso.py
+++ animal_toso.py
@@ -53,18 +53,20 @@ class Enemy(pg.sprite.Sprite):
     """
     imgs = [pg.image.load(f"{MAIN_DIR}/fig/enemy{i}.png") for i in range(1, 6)]
     
+    
 
     def __init__(self):
         super().__init__()
         self.image = random.choice(__class__.imgs)
         self.tmr =random.randint(1, 100)
+        self.vy = random.randint(575, 605)  # 敵出現の縦座標
 
-        self.rect = self.image.get_rect()
-        self.rect.center = 200, 590  # 敵の初期位置
-        self.xy = 2  # 敵キャラの横方向の移動速度
+        self.rect = self.image.get_rect()  # 敵のレクト
+        self.rect.center = 200, self.vy  # 敵の初期位置
+        self.xy = 1  # 敵キャラの横方向の移動速度
         self.state = "normal"  # 状態
-        self.hp = 100
-        self.attack = 20
+        self.hp_enemy = 100
+        self.attack_enemy = 20
 
     def update(self,screen:pg.Surface):
         """
@@ -75,10 +77,38 @@ class Enemy(pg.sprite.Sprite):
             screen.blit(self.image, self.rect)
 
         elif self.state == "stop":
-            screen.blit(self.image, self.rect)   
+            screen.blit(self.image, self.rect) 
+            
+
+        elif self.hp_enemy < 0:
+            self.kill()
             
         else:
             self.state == "normal"
+
+class Attack_effect(pg.sprite.Sprite):
+    """
+    攻撃エフェクトに関するクラス
+    """
+    def __init__(self, obj: "Siro", life: int):
+        """
+        攻撃するエフェクトを生成する
+        引数1 obj：攻撃するSiroまたは
+        引数2 life：発生時間
+        """
+        super().__init__()
+        self.image = pg.image.load(f"{MAIN_DIR}/fig/attack_effect.png")
+        self.rect = self.image.get_rect(center=(obj.rect.centerx+70, obj.rect.centery))
+        self.life = life
+
+    def update(self):
+        """
+        攻撃時間を1減算した攻撃経過時間_lifeに応じて
+        爆発エフェクトを表現する
+        """
+        self.life -= 1
+        if self.life < 0:
+            self.kill()
              
 
 
@@ -88,6 +118,7 @@ def main():
     bg_img = pg.image.load(f"{MAIN_DIR}/fig/haikei.jpg")
 
     emys = pg.sprite.Group()
+    attack_e = pg.sprite.Group()
 
     tmr = 0
     clock = pg.time.Clock()
@@ -95,20 +126,25 @@ def main():
     enemy_siro = Siro(0, 200, 0.4)
     siro = Siro(1, 1400, 0.3)
     while True:
+        frame_enemy = random.randint(300, 500)
         key_lst = pg.key.get_pressed()
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return 0
         
-        if tmr%200 == 0:  # 100フレームに1回敵出現
+        if tmr%frame_enemy == 0:  # randomフレームに1回敵出現
             emys.add(Enemy())
 
         for emy in emys:  # 敵が城前で止まり攻撃
             if len(pg.sprite.spritecollide(siro, [emy], False)) != 0:
                 emy.state = "stop"
                 if tmr%100 == emy.tmr:
-                    siro.hp -= emy.attack
-                print(siro.hp)
+                    emy.rect.move_ip(-10,0)  # 攻撃モーション
+                    attack_e.add(Attack_effect(emy, 3)) # 攻撃エフェクト発生
+                    siro.hp -= emy.attack_enemy  # 城にダメージ
+                    print(siro.hp)
+            else:
+                emy.state ="normal"
    
         
         screen.blit(bg_img, [0, 0])
@@ -116,6 +152,8 @@ def main():
         enemy_siro.update(screen)
         siro.update(screen)
         emys.update(screen)
+        attack_e.update()
+        attack_e.draw(screen)
 
         pg.display.update()
 
