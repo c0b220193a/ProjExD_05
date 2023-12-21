2917eb5a6ede4519854d8636b7dc34c554028c29
C0B22123 <C0B22123d2@edu.teu.ac.jp>
2023-12-21 15:17:07 +0900

    どらごん追加

--- animal_toso.py
+++ animal_toso.py
@@ -38,7 +38,7 @@ class Siro(pg.sprite.Sprite):
         self.image = self.img
         self.rect = self.image.get_rect()
         self.rect.center = zahyo, 550
-        self.hp = 1000
+        self.hp = 5000
 
 
     def update(self, screen: pg.Surface):
@@ -56,7 +56,7 @@ class Enemy(pg.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = random.choice(__class__.imgs)
-        self.tmr =random.randint(5, 100)
+        self.tmr =random.randint(1, 100)
         self.vy = random.randint(575, 605)  # 敵出現の縦座標
         self.speed = random.randint(1, 2)  # 敵のスピード変数
 
@@ -113,7 +113,7 @@ class Boss(pg.sprite.Sprite):
     ボスに関するクラス
     """
 
-    def __init__(self, speed: float):
+    def __init__(self):
         super().__init__()
         self.image = pg.transform.rotozoom(pg.image.load(f"{MAIN_DIR}/fig/boss.png"), 0, 6.0)
         
@@ -122,7 +122,7 @@ class Boss(pg.sprite.Sprite):
 
         self.rect = self.image.get_rect()  # ボスのレクト
         self.rect.center = 200, 460  # ボスの初期位置
-        self.xy = speed # ボスの移動速度
+        self.xy = 1 # ボスの移動速度
         self.state = "normal"  # 状態
         self.hp_enemy = 1000  # ボスのHP
         self.attack_enemy = 100  # ボスの攻撃力
@@ -178,7 +178,6 @@ class Kanban(pg.sprite.Sprite):
         super().__init__()
         img0 = pg.transform.rotozoom(pg.image.load(f"{MAIN_DIR}/fig/kanban.png"), 0, 1)
         self.img = pg.transform.flip(img0, True, False)  # デフォルトの看板
-
         self.image = self.img
         self.rect = self.image.get_rect()
         self.rect.center = x, 620
@@ -189,6 +188,68 @@ class Kanban(pg.sprite.Sprite):
         """
         screen.blit(self.image, self.rect)
 
+
+class Dragon(pg.sprite.Sprite):
+    """
+    ドラゴンに関するクラス
+    """
+    imgs = [pg.image.load(f"{MAIN_DIR}/fig/dragon{i}.png") for i in range(1, 3)]
+    
+    def __init__(self):
+        super().__init__()
+        self.image = random.choice(__class__.imgs)
+        self.tmr =random.randint(1, 100)
+        self.vy = random.randint(575, 605)  # 敵出現の縦座標
+
+        self.rect = self.image.get_rect()  # 敵のレクト
+        self.rect.center = 200, self.vy  # 敵の初期位置
+        self.xy = 1  # 敵キャラの移動速度
+        self.state = "normal"  # 状態
+        self.hp_enemy = 300  # 敵のHP
+        self.attack_enemy = 40  # 敵の攻撃力
+
+    def update(self,screen:pg.Surface):
+        """
+        敵を右から左へ移動
+        """
+        if self.state == "normal":  # 通常状態
+            self.rect.move_ip(self.xy,0)
+            screen.blit(self.image, self.rect)
+
+        elif self.state == "stop":  # 停止状態
+            screen.blit(self.image, self.rect) 
+            
+        elif self.hp_enemy < 0:
+            self.kill()
+            
+        else:
+            self.state == "normal"
+
+
+class Attack_effect_dragon(pg.sprite.Sprite):
+    """
+    ドラゴン攻撃に関するクラス
+    """
+    def __init__(self, obj: "Siro", life: int):
+        """
+        攻撃するエフェクトを生成する
+        引数1 obj：攻撃するSiroまたは
+        引数2 life：発生時間
+        """
+        super().__init__()
+        self.image = pg.image.load(f"{MAIN_DIR}/fig/attack_effect_dragon1.png")
+        self.rect = self.image.get_rect(center=(obj.rect.centerx+150, obj.rect.centery))
+        self.life = life
+
+    def update(self):
+        """
+        攻撃時間を1減算した攻撃経過時間_lifeに応じて
+        攻撃エフェクトを表現する
+        """
+        self.life -= 1
+        if self.life < 0:
+            self.kill()
+
 def main():
     pg.display.set_caption("アニマル闘争")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -197,6 +258,7 @@ def main():
     emys = pg.sprite.Group()
     attack_e = pg.sprite.Group()
     boss = pg.sprite.Group()
+    dragon = pg.sprite.Group()
 
     tmr = 0
     clock = pg.time.Clock()
@@ -204,6 +266,8 @@ def main():
     enemy_siro = Siro(0, 200, 0.4)
     siro = Siro(1, 1400, 0.3)
     kanban = Kanban(1290)
+    bossnum = False
+
     while True:
         frame_enemy = random.randint(300, 500)  # 敵出現頻度ランダム
         key_lst = pg.key.get_pressed()
@@ -211,7 +275,7 @@ def main():
             if event.type == pg.QUIT:
                 return 0
         
-        if tmr%frame_enemy == 0:  # randomフレームに1回敵出現
+        if tmr%600 == frame_enemy:  # randomフレームに1回敵出現
             emys.add(Enemy())
 
         for emy in emys:  # 敵が城前で止まり攻撃
@@ -225,9 +289,9 @@ def main():
             else:
                 emy.state ="normal"
 
-        if siro.hp == 500:
-            boss.add(Boss(0.5))
-            siro.hp -= 10
+        if siro.hp <= 2000 and bossnum is not True:
+            boss.add(Boss())
+            bossnum = True
 
         for bos in boss:  # bossが城前で止まり攻撃
             if len(pg.sprite.spritecollide(siro, [bos], False)) != 0:
@@ -239,6 +303,20 @@ def main():
             else:
                 bos.state = "normal"
 
+        if tmr%1000 == 999:
+            dragon.add(Dragon())
+
+        for dra in dragon:  # 敵が城前で止まり攻撃
+            if len(pg.sprite.spritecollide(siro, [dra], False)) != 0:
+                dra.state = "stop"
+                if tmr%100 == dra.tmr:
+                    dra.rect.move_ip(-10,0)  # 攻撃モーション
+                    attack_e.add(Attack_effect_dragon(dra, 4)) # 攻撃エフェクト発生:数字はエフェクトフレーム
+                    siro.hp -= dra.attack_enemy  # 城にダメージ
+                    print(siro.hp)
+            else:
+                dra.state ="normal"
+
 
         screen.blit(bg_img, [0, 0])
 
@@ -247,6 +325,7 @@ def main():
         kanban.update(screen)
         boss.update(screen)
         emys.update(screen)
+        dragon.update(screen)
         attack_e.update()
         attack_e.draw(screen)
 
